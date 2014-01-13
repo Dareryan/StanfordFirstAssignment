07f1aa2004f06385548286d6266217a5af690a46
Dare Ryan <dareryan@me.com>
2014-01-13 14:52:55 -0500

    Initial Commit

---
+++ StanfordFirstAssignment/StanfordFirstAssignment/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StanfordFirstAssignment
+//
+//  Created by Dare Ryan on 1/13/14.
+//  Copyright (c) 2014 Dare Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
